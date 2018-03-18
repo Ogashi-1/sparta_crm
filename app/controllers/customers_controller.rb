a57f12883cc8d9ceeb25cb0cf33612f0cca10bf0
Shuji <mnksd26sj@outlook.jp>
2018-03-18 15:18:05 +0900

    ransack

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,8 @@ class CustomersController < ApplicationController
 before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
   def index
-    @customers = Customer.page(params[:page])
+    @q = Customer.includes(:post, :company).ransack(params[:q])
+    @customers = @q.result.page(params[:page])
   end
 
   def new
@@ -44,7 +45,7 @@ before_action :set_customer, only: [:show, :edit, :update, :destroy]
   end
 
   def customer_params
-    params.require(:customer).permit(:family_name, :given_name, :email, :company_id
+    params.require(:customer).permit(:family_name, :given_name, :email, :company_id, :post_id, :post_id
     )
   end
 end
